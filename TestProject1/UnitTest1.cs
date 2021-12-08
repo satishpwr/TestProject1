d6e32c4f21ee0d56e356f5afc37c6555679dce31
satish <satishpawar.vision@gmail.com>
2021-12-08 18:33:32 +0530

    Add project files.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class Tests
+    {
+       private IWebDriver _webDriver;
+
+        [SetUp]
+               
+        public void Setup()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            _webDriver = new ChromeDriver();
+            _webDriver.Manage().Window.Maximize();
+            _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _webDriver.Quit();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            _webDriver.Navigate().GoToUrl("https://www.google.com/");
+            _webDriver.FindElement(By.XPath("//div/a[text()='About']")).Click();
+           // String note = _webDriver.FindElement(By.XPath("//h1/[text()='Subscribe to our Newsletter']")).Text;
+            //Thread.Sleep(3000);
+            //Assert.AreEqual("Subscribe to our Newsletter", note);
+        }
+    }
+}
