0e00bb56239dc2ef17a3635493d53c714fd8cbea
satish <satishpawar.vision@gmail.com>
2021-12-09 10:46:38 +0530

    test

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -22,7 +22,7 @@ namespace TestProject1
             new DriverManager().SetUpDriver(new ChromeConfig());
             _webDriver = new ChromeDriver();
             _webDriver.Manage().Window.Maximize();
-            _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3000);
         }
 
         [TearDown]
@@ -34,9 +34,11 @@ namespace TestProject1
         [Test]
         public void Test1()
         {
-            _webDriver.Navigate().GoToUrl("https://www.google.com/");
-            _webDriver.FindElement(By.XPath("//div/a[text()='About']")).Click();
-           // String note = _webDriver.FindElement(By.XPath("//h1/[text()='Subscribe to our Newsletter']")).Text;
+            _webDriver.Navigate().GoToUrl("https://localhost:44310/");
+            IWebElement enter = _webDriver.FindElement(By.TagName("a"));
+            enter.Click();
+            //_webDriver.FindElement(By.XPath("//div/a[text()='About']")).Click();
+            // String note = _webDriver.FindElement(By.XPath("//h1/[text()='Subscribe to our Newsletter']")).Text;
             //Thread.Sleep(3000);
             //Assert.AreEqual("Subscribe to our Newsletter", note);
         }
---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult GetEmployees()
+        {
+            return View("Index", EmployeeStore.FetchEmployees());
+        }
+
+        public ActionResult GetEmployee(int id)
+        {
+            Employee emp = EmployeeStore.EmployeeList.FirstOrDefault(x => x.EmpId == id);
+            return View("EmployeeDetail", emp);
+        }
+
+        [HttpPost]
+        public ActionResult SetEmployee(Employee emp)
+        {
+            Employee employee = EmployeeStore.EmployeeList.FirstOrDefault(x => x.EmpId == emp.EmpId);
+            employee.Name = emp.Name;
+            employee.Salary = emp.Salary;
+            return View("EmployeeDetail", emp);
+        }
+
+        public ActionResult AddEmployee()
+        {
+            return View("AddEmployee");
+        }
+
+        [HttpPost]
+        public ActionResult AddEmployee(Employee emp)
+        {
+            bool IsExist = EmployeeStore.EmployeeList.Any(x => x.Name == emp.Name);
+            if (IsExist)
+            {
+                TempData["test"] = "Name is Already Exist";
+                ViewBag.Message = "Name is Already Exist";
+
+                TempData["msg"] = "<script>alert('Change succesfully');</script>";
+                ViewBag.Javascript = "<script language='javascript' type='text/javascript'>alert('Data Already Exists');</script>";
+                return Json(new { error = true, message = "Name is Already Exist" });
+            }
+            else
+            {
+                EmployeeStore.EmployeeList.Add(new Employee()
+                {
+                    EmpId = EmployeeStore.EmployeeList.Count == 0 ? 1 : EmployeeStore.EmployeeList.Max(x => x.EmpId) + 1,
+                    Name = emp.Name,
+                    Salary = emp.Salary,
+
+                });
+                return Json(new { error = true, message = "Employee saved successfully" });
+            }
+
+            return View("EmployeeDetail", emp);
+        }
+
+        [HttpPost]
+        public void DeleteEmployee(int[] empIds)
+        {
+            EmployeeStore.EmployeeList.RemoveAll(x => empIds.Contains(x.EmpId));
+        }
+    }
+}
